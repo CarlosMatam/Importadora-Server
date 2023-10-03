7e791c20eb33c2083f45336ee61106e1ff7e7396
Carlos Matamoros Luna <carlosmatamorosluna25@gmail.com>
2023-10-03 12:04:48 -0600

    clientes_prueba

--- models/Cliente.js
+++ models/Cliente.js
@@ -2,15 +2,15 @@ import db from "../database/db.js";
 //importamos sequelize
 import { DataTypes } from "sequelize";
 
-const ClienteSModel = db.define('TAB_CLIENTES', {
-    ID_CLIENTE: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-    NOMBRE: { type: DataTypes.STRING },
-    APELLIDO_PATERNO: { type: DataTypes.STRING },
-    APELLIDO_MATERNO: { type: DataTypes.STRING },
-    ID_TIPO_CLIENTE:{type: DataTypes.INTEGER},
-    CORREO: { type: DataTypes.STRING },
-    TIPO_CEDULA: { type: DataTypes.INTEGER },
-    CEDULA: { type: DataTypes.STRING },
+const ClienteSModel = db.define('tab_clientes', {
+    id_cliente: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+    nombre: { type: DataTypes.STRING },
+    apellido_paterno: { type: DataTypes.STRING },
+    apellido_materno: { type: DataTypes.STRING },
+    id_tipo_cliente:{type: DataTypes.INTEGER},
+    correo: { type: DataTypes.STRING },
+    tipo_cedula: { type: DataTypes.INTEGER },
+    cedula: { type: DataTypes.STRING },
 
 })
 
