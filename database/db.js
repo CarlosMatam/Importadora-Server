c874c8820695933bce0d70e4193a23274e4dc10f
Carlos Matamoros Luna <carlosmatamorosluna25@gmail.com>
2023-10-03 02:17:36 -0600

    nombredb

--- database/db.js
+++ database/db.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+const db = new Sequelize(process.env.DB_DBNAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: 'b0ksmfcq5ngfygumb0tt-mysql.services.clever-cloud.com',
 
     dialect: 'mysql'
